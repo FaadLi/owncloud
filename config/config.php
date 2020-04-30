86d998ee7aa591b0b2b81f6c2e0c298d6b70e499
FaadLi <mzfadlii@gmail.com>
2020-04-30 22:09:22 +0700

    original

---
+++ config/config.php
@@ -0,0 +1,37 @@
+<?php
+$CONFIG = array (
+  'passwordsalt' => '1mDUzqtEGjdea+ZF6Jl70qo7B7Qysw',
+  'secret' => 't9f0woPDPCS8mfvP1/V1FmnBQPn0jY6ErZfwWPdPinUHxEno',
+  'trusted_domains' => 
+  array (
+    0 => '192.168.0.109',
+  ),
+  'datadirectory' => '/var/www/html/owncloud/data',
+  'overwrite.cli.url' => 'http://192.168.0.109/owncloud',
+  'dbtype' => 'mysql',
+  'version' => '10.2.1.4',
+  'dbname' => 'owncloud2',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'oc_admin1',
+  'dbpassword' => 'MZI/ME7jSC0yF/2zZFP/OVd9DugUY7',
+  'instanceid' => 'ocegezoksfix',
+  'logtimezone' => 'UTC',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/owncloud/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/owncloud/apps-external',
+      'url' => '/apps-external',
+      'writable' => true,
+    ),
+  ),
+  'installed' => true,
+);
